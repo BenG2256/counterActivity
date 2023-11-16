24d6d9982000420ee20049e0bfedd725e7a31647
Ben Gilley <bengilley5@gmail.com>
2023-11-15 21:07:43 -0500

    functional counter using react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ // const [name, setName] = useState('ben')
+
+ // setTimeout(() => {
+   // setName('charles')
+ // }, 2500)
+
+ const [counter, setCounter] = useState(0)
+ const increment = () => {
+    setCounter(currVal => {
+        return currVal + 1
+
+    })
+ }
+
+ const decrement = () => {
+    setCounter(currVal => {
+        return currVal > 0 ? currVal - 1 : 0;
+
+    })
+ }
+    return (
+        <div>
+           <h1>header</h1>
+            <h3>counter: {counter}</h3>
+            <button onClick={increment}>+</button>
+            <button onClick={decrement}>-</button>
+        </div>
+    );
 }
 
 export default App;
